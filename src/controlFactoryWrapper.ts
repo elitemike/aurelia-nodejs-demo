687021b4464549308ae80e862ab836902291825e
Mike Stankovich <mike.stankovich@outlook.com>
2020-02-06 19:37:56 -0500

    updates

--- src/controlFactoryWrapper.ts
+++ src/controlFactoryWrapper.ts
@@ -1,24 +1,24 @@
+import { BindingEngine } from "aurelia-binding";
 import { initialize } from "aurelia-pal-nodejs";
 import "aurelia-polyfills";
-import { Container, autoinject, inject } from "aurelia-dependency-injection";
-import { ControlFactory } from "custom-controls";
+import { Container, autoinject } from "aurelia-dependency-injection";
 import { configure as configureBindingLanguage } from "aurelia-templating-binding";
 import { configure as configureValidation } from "aurelia-validation";
 
 @autoinject
 export class ControlFactoryWrapper {
   constructor(
-    private customControlFactory: ControlFactory,
+    private bindingEngine: BindingEngine,
     private container: Container
   ) {
-    //  console.debug("args", arguments);
     initialize();
     configureBindingLanguage({ container });
     configureValidation({ container });
   }
 
   procesJson() {
-    console.log("cfw class", this.customControlFactory);
-    return this.customControlFactory;
+    // console.log("cfw class", this.customControlFactory);
+    // return this.customControlFactory;
+    return "processed";
   }
 }
